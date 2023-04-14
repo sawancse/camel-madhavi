d1357bdd253720f77f83d12e4b747b621c3dbcaf
sawancse <sawancse@gmail.com>
2023-04-14 08:32:00 +0530

    Add files via upload

--- learncamel-spring-boot-ldap/src/main/java/com/learncamel/LearncamelSpringBootApplication.java
+++ learncamel-spring-boot-ldap/src/main/java/com/learncamel/LearncamelSpringBootApplication.java
@@ -2,8 +2,10 @@ package com.learncamel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:bean.xml")
 public class LearncamelSpringBootApplication {
 
 	public static void main(String[] args) {
--- learncamel-spring-boot-ldap/src/main/java/com/learncamel/route/SimpleCamelRoute.java
+++ learncamel-spring-boot-ldap/src/main/java/com/learncamel/route/SimpleCamelRoute.java
@@ -6,18 +6,13 @@ import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
-import com.learncamel.bean.MyBean;
-import com.learncamel.exception.CamelCustomException;
-import com.learncamel.exception.GenericCamelCustomException;
-import com.learncamel.processor.MyProcessor;
-import com.learncamel.processor.ProcessorCustomException;
 /*https://camel.apache.org/components/3.20.x/ldap-component.html*/
 @Component
 public class SimpleCamelRoute extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 
-		from("file:data/input?delete=true&readLock=none").process(new Processor() {
+		from("timer:hello?period=20s").process(new Processor() {
 
 			@Override
 			public void process(Exchange exchange) throws Exception {
@@ -26,7 +21,7 @@ public class SimpleCamelRoute extends RouteBuilder {
 			}
 		}).to("direct:ldapcall")
 /*		.to("ldap:ldapserver?base=ou=mygroup,ou=groups,ou=system,member=uid=huntc,ou=users,ou=system").log("ldap res ${body}");
-*/		.to("ldap:ldapserver?base=ou=mathematicians").log("ldap res ${body}");
+*/		.to("ldap:ldapserver?base=ou=scientists,dc=example,dc=com").log("ldap res ${body}");
 
 
 		from("direct:ldapcall").process(new Processor() {
