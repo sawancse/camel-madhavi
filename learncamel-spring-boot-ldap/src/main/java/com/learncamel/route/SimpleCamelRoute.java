0f3acf4cdd0f22e15482aca77e6c8c10326c5234
sawancse <sawancse@gmail.com>
2023-04-20 08:49:54 +0530

    Add files via upload

--- learncamel-spring-boot-ldap/src/main/java/com/learncamel/route/SimpleCamelRoute.java
+++ learncamel-spring-boot-ldap/src/main/java/com/learncamel/route/SimpleCamelRoute.java
@@ -1,8 +1,16 @@
 package com.learncamel.route;
 
+import java.security.KeyStore.Entry.Attribute;
+import java.util.Collection;
+import java.util.Iterator;
+
+import javax.naming.directory.Attributes;
+import javax.naming.directory.SearchResult;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.Header;
 import org.apache.camel.Processor;
+import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -12,31 +20,46 @@ public class SimpleCamelRoute extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 
-		from("timer:hello?period=20s").process(new Processor() {
+		from("timer:hello?period=30s").process(new Processor() {
 
 			@Override
 			public void process(Exchange exchange) throws Exception {
 				// TODO Auto-generated method stub
-
+				//.getIn().setBody("(ou=mathematicians)");
+				exchange.getIn().setBody("(objectClass=*)");
+				// exchange.getIn().setHeader("objectClass","person");
+				// exchange.getIn().setHeader("sn","Euler");
 			}
-		}).to("direct:ldapcall")
-/*		.to("ldap:ldapserver?base=ou=mygroup,ou=groups,ou=system,member=uid=huntc,ou=users,ou=system").log("ldap res ${body}");
-*/		///.to("ldap:ldapserver?base=ou=mathematicians").log("ldap res ${body}");   //step-1
-		.to("ldap:ldapserver?base=uid=boyle,dc=example,dc=com").log("ldap res ${body}");   //step-1
-		//writing to csv
-		//
-		
-//"ldap:ldapserver?base=ou=mygroup,ou=groups,ou=system",
-	//    "(member=uid=huntc,ou=users,ou=system)"
-
-		from("direct:ldapcall").process(new Processor() {
-
+		})
+		//.to("direct:ldapcall")
+		.to("ldap:ldapserver?base=uid=pasteur,dc=example,dc=com&scope=subtree")
+		//.to("ldap:ldapserver?base=dc=example,dc=com&scope=subtree")  
+		.log("ldap res ${body}")
+		.process(new Processor() {
+			
 			@Override
 			public void process(Exchange exchange) throws Exception {
 				// TODO Auto-generated method stub
-
+				 Collection<SearchResult> data =  exchange.getIn().getBody(Collection.class);
+				 System.out.println("data: " + data);
+				 
+				 Iterator resultIter = data.iterator();
+				  SearchResult searchResult = (SearchResult) resultIter.next();
+				  Attributes attributes = searchResult.getAttributes();
+				  javax.naming.directory.Attribute deviceCNAttr = attributes.get("cn");
+				  javax.naming.directory.Attribute deviceMailAttr = attributes.get("mail");
+				  javax.naming.directory.Attribute deviceTelphoneAttr = attributes.get("telephoneNumber");
+				  String cn = (String) deviceCNAttr.get();attributes.get("cn");
+				  String mail = (String) deviceMailAttr.get();attributes.get("mail");
+				  String telephoneNumber = (String) deviceTelphoneAttr.get();attributes.get("telephoneNumber");
+				  String output= cn +"|" + mail+"|"+telephoneNumber;
+				  System.out.println(output);
+				  exchange.getIn().setBody(output);
+				
 			}
-		});
+		})
+		  .to("file:data/output?fileName=ldap.csv");
+
 
 	}
 }
