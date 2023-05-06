dcda98b91e2c26b5885ea9f450c0eb35dce1f9b4
sawancse <sawancse@gmail.com>
2023-05-06 08:09:35 +0530

    Add files via upload

--- learncamel-spring-boot-ldap/src/main/java/com/learncamel/route/SimpleCamelRoute.java
+++ learncamel-spring-boot-ldap/src/main/java/com/learncamel/route/SimpleCamelRoute.java
@@ -1,9 +1,13 @@
 package com.learncamel.route;
 
 import java.security.KeyStore.Entry.Attribute;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 
+import javax.naming.NamingEnumeration;
 import javax.naming.directory.Attributes;
 import javax.naming.directory.SearchResult;
 
@@ -20,46 +24,99 @@ public class SimpleCamelRoute extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 
+		
+		/*
+		 * onException(Exception.class).process(new Processor() {
+		 * 
+		 * public void process(Exchange exchange) throws Exception {
+		 * log.info("handling ex 1"); }
+		 * }).log("Exception body ${body} ").handled(true).to("file:data/error");
+		 */
+		
 		from("timer:hello?period=30s").process(new Processor() {
 
 			@Override
 			public void process(Exchange exchange) throws Exception {
-				// TODO Auto-generated method stub
-				//.getIn().setBody("(ou=mathematicians)");
-				exchange.getIn().setBody("(objectClass=*)");
-				// exchange.getIn().setHeader("objectClass","person");
-				// exchange.getIn().setHeader("sn","Euler");
+
+				exchange.getIn().setBody("(ou=scientists)"); // working
+
 			}
-		})
-		//.to("direct:ldapcall")
-		.to("ldap:ldapserver?base=uid=pasteur,dc=example,dc=com&scope=subtree")
-		//.to("ldap:ldapserver?base=dc=example,dc=com&scope=subtree")  
-		.log("ldap res ${body}")
-		.process(new Processor() {
-			
+		}).to("ldap:ldapserver?base=dc=example,dc=com&scope=subtree").process(new Processor() {
+
 			@Override
 			public void process(Exchange exchange) throws Exception {
 				// TODO Auto-generated method stub
-				 Collection<SearchResult> data =  exchange.getIn().getBody(Collection.class);
-				 System.out.println("data: " + data);
-				 
-				 Iterator resultIter = data.iterator();
-				  SearchResult searchResult = (SearchResult) resultIter.next();
-				  Attributes attributes = searchResult.getAttributes();
-				  javax.naming.directory.Attribute deviceCNAttr = attributes.get("cn");
-				  javax.naming.directory.Attribute deviceMailAttr = attributes.get("mail");
-				  javax.naming.directory.Attribute deviceTelphoneAttr = attributes.get("telephoneNumber");
-				  String cn = (String) deviceCNAttr.get();attributes.get("cn");
-				  String mail = (String) deviceMailAttr.get();attributes.get("mail");
-				  String telephoneNumber = (String) deviceTelphoneAttr.get();attributes.get("telephoneNumber");
-				  String output= cn +"|" + mail+"|"+telephoneNumber;
-				  System.out.println(output);
-				  exchange.getIn().setBody(output);
+				Collection<SearchResult> data = exchange.getIn().getBody(Collection.class);
+				System.out.println("data: " + data);
+
+				Iterator resultIter = data.iterator();
+				SearchResult searchResult = (SearchResult) resultIter.next();
+				Attributes attributes = searchResult.getAttributes();
+				System.out.println("uniqueMember + " + attributes.get("uniqueMember"));
+				List l = new ArrayList();
+				for (int i = 0; i < attributes.get("uniqueMember").size(); i++) {
+					String uniqueMember = (String) attributes.get("uniqueMember").get(i);
+					l.add(uniqueMember);
+				}
+				exchange.getIn().setBody(l);
+
+				//
+			}
+		}).split(body()).to("direct:fetchUidfromLdap");
+
+		from("direct:fetchUidfromLdap").process(new Processor() {
+
+			@Override
+			public void process(Exchange exchange) throws Exception {
+				// TODO Auto-generated method stub
+				String uid = exchange.getIn().getBody(String.class);
+				String s[] = uid.split(",");
+				String string1[] = s[0].split("=");
+				// System.out.println("string1 " + string1[1]);
+				exchange.getIn().setBody("uid=" + string1[1]);
+
+			}
+		}).to("ldap:ldapserver?base=dc=example,dc=com&scope=subtree").log("body ${body}").to("direct:ldapresponse");
+
+		from("direct:ldapresponse").process(new Processor() {
+
+			@Override
+			public void process(Exchange exchange) throws Exception {
+				// TODO Auto-generated method stub
+				StringBuilder sbUidDetails = new StringBuilder();
+				Collection<SearchResult> data = exchange.getIn().getBody(Collection.class);
+				Iterator resultIter = data.iterator();
+				SearchResult searchResult = (SearchResult) resultIter.next();
+				Attributes attributes = searchResult.getAttributes();
+				javax.naming.directory.Attribute uidAttr = attributes.get("uid");
+				String uid = (String) uidAttr.get();
+				javax.naming.directory.Attribute mailAttr = attributes.get("mail");
+				String mail = (String) mailAttr.get();
+
+				javax.naming.directory.Attribute telephoneNumberAttr = attributes.get("cn");
+				if(telephoneNumberAttr ==null)
+					return;
+				String telephoneNumber = (String) telephoneNumberAttr.get();
+				if(telephoneNumber == null)
+					telephoneNumber = "NA";
 				
+				sbUidDetails.append(uid);
+				sbUidDetails.append(",");
+				sbUidDetails.append(mail);
+			    sbUidDetails.append(",");
+				sbUidDetails.append(telephoneNumber);
+				System.out.println("sbUidDetails + " + sbUidDetails);
+				/* NamingEnumeration<?> attributesEnum = null;
+				 
+				javax.naming.directory.Attribute attribute = attributes.get("telephoneNumber");
+				    if (attribute == null) return;
+				    attributesEnum = attribute.getAll();
+				    Collections.list(attributesEnum).stream().map(Object::toString);*/
+				    exchange.getIn().setBody(sbUidDetails+"\n");
+				    
 			}
 		})
-		  .to("file:data/output?fileName=ldap.csv");
-
-
+		.log("body ${body}")
+		 .to("file:data/output/?fileName=ldap.csv&fileExist=Append&charset=utf-8&Noop=false");
 	}
 }
